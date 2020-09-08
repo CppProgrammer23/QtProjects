b5ff335bb61783f73063acc8bf3fe6a9b1d2aa42
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-09-08 10:26:35 +0100

    Create machinehealth.h

---
+++ QMachine_Check_Up/machinehealth.h
@@ -0,0 +1,65 @@
+#ifndef MACHINEHEALTH_H
+#define MACHINEHEALTH_H
+
+//#pragma warning(disable:4996)
+#include <QMainWindow>
+#include <chrono>
+#include <QDateTimeEdit>
+#include <QTimer>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MachineHealth; }
+QT_END_NAMESPACE
+
+class MachineHealth : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MachineHealth(QWidget *parent = nullptr);
+    ~MachineHealth();
+
+private slots:
+
+    void on_comboBox_currentTextChanged(const QString &arg1);
+
+    void set12hour();
+
+    void set24hour();
+
+    void on_pushButton_clear_clicked();
+
+
+
+    void on_pushButton_clicked();
+
+    void on_actionNew_2_triggered();
+
+    void on_actionClose_triggered();
+
+    void on_actionVersion_triggered();
+
+    void on_pushButton_save_clicked();
+
+    bool State_Ok();
+
+    void Incheck_Boxes();
+
+    void on_actionSave_triggered();
+
+    void on_actionContact_SOS_triggered();
+
+    void update();
+
+private:
+    Ui::MachineHealth *ui;
+    bool Format=false;
+    QDate iD, myD;
+    QTime iT, myT;
+    int t;
+    QTimer* updateTime=new QTimer;
+
+    bool set12=true, set24=false;
+};
+#endif // MACHINEHEALTH_H
