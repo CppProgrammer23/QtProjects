df5db81de9003c6c896f7d562ca3202a66338f6e
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-09-15 17:50:26 +0100

    Update qvideoplayer.h

--- QVideoPlayer/qvideoplayer.h
+++ QVideoPlayer/qvideoplayer.h
@@ -7,7 +7,6 @@
 #include <QMediaMetaData>
 #include <QTime>
 #include <QVideoWidget>
-#include <map>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class QVideoPlayer; }
@@ -22,7 +21,6 @@ public:
     ~QVideoPlayer();
 
     void timeChanged(qint64);
-    //void durationChanged(qint64);
 
 private slots:
     void on_play_pausebutton_clicked();
@@ -30,6 +28,8 @@ private slots:
     void on_horizontalSlider_time_sliderMoved(int position);
 
     void on_soundSlider_sliderMoved(int position);
+
+    void on_soundSlider_valueChanged(int value);
 
     void on_pushButton_clicked();
 
@@ -45,13 +45,26 @@ private slots:
 
     void on_back_button_clicked();
 
+    void on_play_pausebutton_pressed();
+
+    void on_play_pausebutton_released();
+
+    void on_nextbutton_pressed();
+
+    void on_nextbutton_released();
+
+    void on_back_button_pressed();
+
+    void on_back_button_released();
+
+    void on_fullScreen_clicked();
+
 private:
     Ui::QVideoPlayer *ui;
     QMediaPlayer *player;
-    bool play, mute;
+    bool play, mute, full;
     qreal linearVolume;
-    std::map<QString,QString> avSongs;
-    std::map<QString,QString>::iterator it;
-    int i = 0;
+    QMap<QString,QString> avSongs;
+    QMap<QString,QString>::iterator it;
 };
 #endif // QVIDEOPLAYER_H
